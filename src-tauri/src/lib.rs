616bd742685b2fa80e0ea2585e8335dd282df0cb
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2026-01-06 17:56:59 +0300

    feat: add `audio` control methods

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
+mod audio;
 mod synth;
 
+use audio::{audio_is_running, audio_start, audio_stop, AudioEngine};
 use synth::{synth_get_state, synth_reset, synth_set_state, SynthEngine};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -7,10 +9,14 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(SynthEngine::default())
+        .manage(AudioEngine::default())
         .invoke_handler(tauri::generate_handler![
             synth_get_state,
             synth_set_state,
-            synth_reset
+            synth_reset,
+            audio_start,
+            audio_stop,
+            audio_is_running
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
